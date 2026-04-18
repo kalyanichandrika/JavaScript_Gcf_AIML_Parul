59a7ce828fe5ea14075bc1f5243e41ad1a6c310c
kalyanichandrika <kalyanichandrika686@gmail.com>
2026-04-18 10:02:47 +0530

    Add files via upload
    commit done files uploaded

---
+++ dom2.js
@@ -0,0 +1,6 @@
+var myh1=document.querySelector('h1');
+    myh1.style.color = 'red';
+    myh1.style.backgroundColor='LightGray'
+    hi=document.createElement("h1")
+       hi.innerText="AIML Branch"
+         document.body.appendChild(hi)
